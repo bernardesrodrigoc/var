18ef5ed507bc3137716ff7dfdd774b9ce7ad38d8
Rodrigo Cupertino Bernardes <bernardesrodrigoc@gmail.com>
2025-12-05 15:19:27 -0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = "https://var-production.up.railway.app"; 
+
 const API_BASE = `${BACKEND_URL}/api`;
 
 // Create axios instance
